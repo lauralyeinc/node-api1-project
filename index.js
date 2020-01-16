5c8003651f4bb09b7054c962215461e0c354d43e
Laura <theimer.laura@gmail.com>
2020-01-15 21:16:52 -0500

    initial set up, and have server 4000 running.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+
+const express = require('express'); 
+
+const db = require("./data/db.js");
+
+const port = 4000;
+
+const server = express(); 
+
+server.listen(port, () => {
+    console.log('**** listening on port 4000');
+});
+
+// global middleware 
+server.use(express.json());   //??  do I need this??
+
+server.get('/', (req, res) => {
+    res.send('hello from express!');
+}); 
+
+/*
+| POST   | /api/users     | Creates a user using the information sent inside the `request body`.
+*/  
+server.post('/api/users', (req, res) => {
+    
+})
